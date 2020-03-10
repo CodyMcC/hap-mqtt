50be2db7368c41404abe8b9325236b9bd5f95a44
codymcco <codymccomber@gmail.com>
2020-03-10 13:40:02 -0700

    Major update

--- accessories/basic_light.py
+++ accessories/basic_light.py
@@ -3,6 +3,8 @@ from pyhap.accessory import Accessory
 
 from pyhap.const import CATEGORY_LIGHTBULB
 
+import paho.mqtt.client as mqtt
+
 import logging
 
 logger = logging.getLogger(__name__)
@@ -12,14 +14,27 @@ class BasicLight(Accessory):
 
     category = CATEGORY_LIGHTBULB
 
-    def __init__(self, *args, pin=11, **kwargs):
+    def __init__(self, location, mqtt_server, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.location = location
+        self.mqtt_server = mqtt_server
+        self.client = mqtt.Client()
+        self.client.connect(mqtt_server, 1883)
+        self.topic = f"{self.location}/{self.display_name}/{self.aid}/Lightbulb"
 
         serv_light = self.add_preload_service('Lightbulb')
+
         self.char_on = serv_light.configure_char('On', setter_callback=self.set_bulb)
+        self.set_info_service(firmware_revision=1, manufacturer="MonikerTechnology",
+                              model="Alpha", serial_number=str(self.aid))
 
     def __setstate__(self, state):
         self.__dict__.update(state)
 
     def set_bulb(self, value):
         logger.info(f"{self.display_name} got an action: {value} [AID: {self.aid}]")
+        value = '{"On": 0}'.replace("0", str(value))
+
+        self.client.publish(self.topic, value)
+
+
--- example.py
+++ example.py
@@ -1,5 +1,7 @@
 from pyhap.accessory_driver import AccessoryDriver
-from hap_mqtt import MqttAccessories, HapMqtt
+from hap_mqtt import HapMqtt
+from pyhap.accessory import Accessory
+import pyhap.loader as loader
 import signal
 import logging
 from os.path import expanduser
@@ -8,6 +10,8 @@ from os.path import expanduser
 from accessories.temperature_sensor import TemperatureSensor
 from accessories.basic_light import BasicLight
 
+
+MQTTSERVER = "192.168.1.132"
 accessory_state = expanduser('~/Documents/2. Code/2. Python/HAP-MQTT/accessory.state')
 
 logger = logging.getLogger(__name__)
@@ -19,19 +23,42 @@ logging.basicConfig(level=logging.INFO,
 driver = AccessoryDriver(port=51827,
                          persist_file=accessory_state)
 
-mqtt_bridge = HapMqtt(driver, "mqtt_bridge")
+mqtt_bridge = HapMqtt(MQTTSERVER, driver, "mqtt_bridge")
+
+
+# get loaders
+service_loader = loader.get_loader()
+char_loader = loader.get_loader()
+
+test = Accessory(driver, "Switch", aid=999196)
+test.add_service(service_loader.get_service("Switch"))
+# MqttAccessories(test)
+# mqtt_bridge.add_accessory(test)
 
 # Create accessories
-MqttAccessories("Outside", BasicLight(driver, "different", aid=999399))
-MqttAccessories("Outside", BasicLight(driver, "Flood_1", aid=1234))
-MqttAccessories("Garage", TemperatureSensor(driver, "Battery_1", aid=2323))
+# MqttAccessories("Outside", BasicLight(driver, "different", aid=999399))
+# MqttAccessories(BasicLight("outside", MQTTSERVER, driver, "flood_1", aid=1111))
+# MqttAccessories(BasicLight("outside", MQTTSERVER, driver, "flood_2", aid=2222))
+# MqttAccessories(BasicLight("outside", MQTTSERVER, driver, "flood_3", aid=3333))
+# MqttAccessories(BasicLight("outside", MQTTSERVER, driver, "flood_4", aid=4444))
+
+accessories = list()
+accessories.append(BasicLight("outside", MQTTSERVER, driver, "flood_1", aid=1111))
+accessories.append(BasicLight("outside", MQTTSERVER, driver, "flood_2", aid=2222))
+accessories.append(BasicLight("outside", MQTTSERVER, driver, "flood_3", aid=3333))
+accessories.append(BasicLight("outside", MQTTSERVER, driver, "flood_4", aid=4444))
+# MqttAccessories(TemperatureSensor(MQTTSERVER, driver, "Battery_1", aid=2323))
+# MqttAccessories.accessories[2].add_service(service_loader.get_service("BatteryService"))
 
 
 # Add the accessories and the topics to the mqtt bridge
-for acc in MqttAccessories.accessories:
+for acc in accessories:
     mqtt_bridge.add_accessory(acc)
-for topic in MqttAccessories.topics:
-    mqtt_bridge.add_topic(topic)
+    mqtt_bridge.add_topic(acc.topic)
+# for topic in MqttAccessories.topics:
+#     mqtt_bridge.add_topic(topic)
+
+
 
 driver.add_accessory(accessory=mqtt_bridge)
 
